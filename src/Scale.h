9a59e5d61f8e4f3d56c92373788c835a736ff58f
Harald Reinmüller <28454466+rmh78@users.noreply.github.com>
2022-03-20 16:25:44 +0100

    remove scale init outputs

--- src/Scale.h
+++ src/Scale.h
@@ -58,23 +58,6 @@ void Scale::init()
     scale.set_scale(SCALE_VALUE);
     // reset the scale to 0
     scale.tare();
-
-    // print a raw reading from the ADC
-    Serial.print("read: \t\t");
-    Serial.println(scale.read());
-
-    // print the average of 20 readings from the ADC
-    Serial.print("read average: \t\t");
-    Serial.println(scale.read_average(20));
-
-    // print the average of 5 readings from the ADC minus the tare weight, set with tare()
-    Serial.print("get value: \t\t");
-    Serial.println(scale.get_value(5));
-
-    // print the average of 5 readings from the ADC minus tare weight, divided
-    // by the SCALE parameter set with set_scale
-    Serial.print("get units: \t\t");
-    Serial.println(scale.get_units(5), 1);
 }
 
 float Scale::readWeight()
